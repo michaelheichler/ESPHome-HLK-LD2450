5842520488d16350d1b8ebf6969b438287306316
Michael Heichler <michaelheichler@protonmail.com>
2024-12-05 15:36:44 +0100

    gdafgd

--- components/LD2450/LD2450.cpp
+++ components/LD2450/LD2450.cpp
@@ -7,35 +7,47 @@ namespace esphome::ld2450
 
     static const char *TAG = "LD2450";
 
-    void LD2450::setup() {
-        // Setup implementation
-    }
-
-    void LD2450::loop() {
-        // Loop implementation
-    }
-
-    void LD2450::dump_config() {
-        // Config dump implementation
-    }
-
-    void LD2450::set_zone(uint8_t zone_index, const std::vector<std::pair<float, float>> &new_polygon) {
-        if (zone_index >= this->zones_.size()) {
-            ESP_LOGW(TAG, "Zone index %d out of bounds", zone_index);
-            return;
-        }
-        if (new_polygon.size() < 3) {
-            ESP_LOGW(TAG, "Polygon must have at least 3 points");
-            return;
+    void LD2450::setup()
+    {
+        for (int i = targets_.size(); i < 3; i++)
+        {
+            Target *new_target = new Target();
+            targets_.push_back(new_target);
         }
 
-        ESP_LOGI(TAG, "Updating Zone %d dynamically", zone_index);
-        this->zones_[zone_index]->polygon = new_polygon;
-        this->send_zone_configuration(zone_index);
+        for (int i = 0; i < targets_.size(); i++)
+        {
+            Target *target = targets_[i];
+            if (target->get_name() == nullptr)
+            {
+                std::string name = std::string("Target ").append(std::to_string(i + 1));
+                char *cstr = new char[name.length() + 1];
+                std::strcpy(cstr, name.c_str());
+                target->set_name(cstr);
+            }
+
+            target->set_fast_off_detection(fast_off_detection_);
+        }
+
+#ifdef USE_BINARY_SENSOR
+        if (occupancy_binary_sensor_ != nullptr)
+            occupancy_binary_sensor_->publish_initial_state(false);
+#endif
+#ifdef USE_BUTTON
+        if (restart_button_ != nullptr)
+            restart_button_->add_on_press_callback([this]()
+                                                   { this->perform_restart(); });
+        if (factory_reset_button_ != nullptr)
+            factory_reset_button_->add_on_press_callback([this]()
+                                                         { this->perform_factory_reset(); });
+#endif
+        read_switch_states();
     }
 
-    void LD2450::send_zone_configuration(uint8_t zone_index) {
-        if (zone_index >= this->zones_.size()) {
+    void LD2450::send_zone_configuration(uint8_t zone_index)
+    {
+        if (zone_index >= this->zones_.size())
+        {
             ESP_LOGW(TAG, "Zone index %d out of bounds", zone_index);
             return;
         }
@@ -44,12 +56,15 @@ namespace esphome::ld2450
         const auto &polygon = zone->polygon;
 
         std::vector<uint8_t> uart_data;
+
         uart_data.push_back(0xFD);
         uart_data.push_back(0xFC);
+
         uart_data.push_back(zone_index);
         uart_data.push_back(polygon.size());
 
-        for (const auto &point : polygon) {
+        for (const auto &point : polygon)
+        {
             int16_t x = static_cast<int16_t>(point.first * 100);
             int16_t y = static_cast<int16_t>(point.second * 100);
             uart_data.push_back((x >> 8) & 0xFF);
@@ -64,48 +79,76 @@ namespace esphome::ld2450
         ESP_LOGI(TAG, "Zone %d configuration sent via UART", zone_index);
     }
 
-    void LD2450::log_sensor_version() {
-        // Log version implementation
+    void LD2450::set_zone(uint8_t zone_index, const std::vector<std::pair<float, float>> &new_polygon)
+    {
+        if (zone_index >= this->zones_.size())
+        {
+            ESP_LOGW(TAG, "Zone index %d out of bounds", zone_index);
+            return;
+        }
+        if (new_polygon.size() < 3)
+        {
+            ESP_LOGW(TAG, "Polygon must have at least 3 points");
+            return;
+        }
+
+        ESP_LOGI(TAG, "Updating Zone %d dynamically", zone_index);
+        this->zones_[zone_index]->polygon = new_polygon;
+
+        this->send_zone_configuration(zone_index);
     }
 
-    void LD2450::log_bluetooth_mac() {
-        // Log MAC address implementation
+    void LD2450::loop()
+    {
+        // Loop implementation (unchanged for brevity)
     }
 
-    void LD2450::perform_restart() {
-        // Restart implementation
+    void LD2450::process_message(uint8_t *msg, int len)
+    {
+        // Implementation for processing messages (unchanged for brevity)
     }
 
-    void LD2450::perform_factory_reset() {
-        // Factory reset implementation
+    void LD2450::process_config_message(uint8_t *msg, int len)
+    {
+        // Implementation for processing config messages (unchanged for brevity)
     }
 
-    void LD2450::set_tracking_mode(bool mode) {
-        // Set tracking mode implementation
+    void LD2450::perform_restart()
+    {
+        const uint8_t restart[2] = {COMMAND_RESTART, 0x00};
+        send_config_message(restart, 2);
+        read_switch_states();
     }
 
-    void LD2450::set_bluetooth_state(bool state) {
-        // Set Bluetooth state implementation
+    void LD2450::perform_factory_reset()
+    {
+        const uint8_t reset[2] = {COMMAND_FACTORY_RESET, 0x00};
+        send_config_message(reset, 2);
+        perform_restart();
     }
 
-    void LD2450::read_switch_states() {
-        // Read switch states implementation
+    void LD2450::set_tracking_mode(bool mode)
+    {
+        if (mode)
+        {
+            const uint8_t set_tracking_mode[2] = {COMMAND_MULTI_TRACKING_MODE, 0x00};
+            send_config_message(set_tracking_mode, 2);
+        }
+        else
+        {
+            const uint8_t set_tracking_mode[2] = {COMMAND_SINGLE_TRACKING_MODE, 0x00};
+            send_config_message(set_tracking_mode, 2);
+        }
+
+        const uint8_t request_tracking_mode[2] = {COMMAND_READ_TRACKING_MODE, 0x00};
+        send_config_message(request_tracking_mode, 2);
     }
 
-    void LD2450::set_baud_rate(BaudRate baud_rate) {
-        // Set baud rate implementation
-    }
-
-    void LD2450::process_message(uint8_t *msg, int len) {
-        // Process message implementation
-    }
-
-    void LD2450::process_config_message(uint8_t *msg, int len) {
-        // Process config message implementation
-    }
-
-    void LD2450::write_command(uint8_t *msg, int len) {
-        // Write command implementation
+    void LD2450::read_switch_states()
+    {
+        const uint8_t request_tracking_mode[2] = {COMMAND_READ_TRACKING_MODE, 0x00};
+        send_config_message(request_tracking_mode, 2);
+        log_bluetooth_mac();
     }
 
 } // namespace esphome::ld2450
--- components/LD2450/LD2450.h
+++ components/LD2450/LD2450.h
@@ -22,132 +22,20 @@
 #include "esphome/components/button/button.h"
 #endif
 
-#define SENSOR_UNAVAILABLE_TIMEOUT 4000
-#define CONFIG_RECOVERY_INTERVAL 60000
-#define POST_RESTART_LOCKOUT_DELAY 2000
-
-#define COMMAND_MAX_RETRIES 10
-#define COMMAND_RETRY_DELAY 100
-
-#define COMMAND_ENTER_CONFIG 0xFF
-#define COMMAND_LEAVE_CONFIG 0xFE
-#define COMMAND_READ_VERSION 0xA0
-#define COMMAND_RESTART 0xA3
-#define COMMAND_FACTORY_RESET 0xA2
-
-#define COMMAND_READ_TRACKING_MODE 0x91
-#define COMMAND_SINGLE_TRACKING_MODE 0x80
-#define COMMAND_MULTI_TRACKING_MODE 0x90
-
-#define COMMAND_READ_MAC 0xA5
-#define COMMAND_BLUETOOTH 0xA4
-
-#define COMMAND_SET_BAUD_RATE 0xA1
-
 namespace esphome::ld2450
 {
 
-    enum BaudRate
-    {
-        BAUD_9600 = 0x01,
-        BAUD_19200 = 0x02,
-        BAUD_38400 = 0x03,
-        BAUD_57600 = 0x04,
-        BAUD_115200 = 0x05,
-        BAUD_230400 = 0x06,
-        BAUD_256000 = 0x07,
-        BAUD_460800 = 0x08,
-    };
-
-    static const std::map<std::string, BaudRate> BAUD_STRING_TO_ENUM{
-        {"9600", BAUD_9600},
-        {"19200", BAUD_19200},
-        {"38400", BAUD_38400},
-        {"57600", BAUD_57600},
-        {"115200", BAUD_115200},
-        {"230400", BAUD_230400},
-        {"256000", BAUD_256000},
-        {"460800", BAUD_460800}};
-
-    class TrackingModeSwitch;
-    class BluetoothSwitch;
-    class BaudRateSelect;
-
-    class EmptyButton : public button::Button
-    {
-    protected:
-        virtual void press_action() {};
-    };
-
     class LD2450 : public uart::UARTDevice, public Component
     {
-#ifdef USE_BINARY_SENSOR
-        SUB_BINARY_SENSOR(occupancy)
-#endif
-#ifdef USE_SENSOR
-        SUB_SENSOR(target_count)
-#endif
-#ifdef USE_NUMBER
-        SUB_NUMBER(max_distance)
-        SUB_NUMBER(max_angle)
-        SUB_NUMBER(min_angle)
-#endif
-#ifdef USE_BUTTON
-        SUB_BUTTON(restart)
-        SUB_BUTTON(factory_reset)
-#endif
     public:
         void setup() override;
         void loop() override;
-        void dump_config() override;
-        void set_zone(uint8_t zone_index, const std::vector<std::pair<float, float>> &new_polygon);
-        void log_sensor_version();
-        void log_bluetooth_mac();
-        void perform_restart();
-        void perform_factory_reset();
-        void set_tracking_mode(bool mode);
-        void set_bluetooth_state(bool state);
-        void read_switch_states();
-        void set_baud_rate(BaudRate baud_rate);
-
-    private:
         void send_zone_configuration(uint8_t zone_index);
+        void set_zone(uint8_t zone_index, const std::vector<std::pair<float, float>> &new_polygon);
 
     protected:
         void process_message(uint8_t *msg, int len);
         void process_config_message(uint8_t *msg, int len);
-        void write_command(uint8_t *msg, int len);
-        void send_config_message(const uint8_t *msg, int len)
-        {
-            command_queue_.push_back(std::vector<uint8_t>(msg, msg + len));
-        }
-
-        uint8_t peek_status_ = 0;
-        const char *name_ = "LD2450";
-        bool flip_x_axis_ = false;
-        bool is_occupied_ = false;
-        bool fast_off_detection_ = false;
-        bool configuration_mode_ = false;
-        bool is_applying_changes_ = false;
-        bool sensor_available_ = false;
-        int configuration_message_length_ = 0;
-        uint32_t command_last_sent_ = 0;
-        uint32_t last_message_received_ = 0;
-        uint32_t last_available_change_ = 0;
-        uint32_t last_config_leave_attempt_ = 0;
-        uint32_t apply_change_lockout_ = 0;
-        int last_available_size_ = 0;
-        std::vector<std::vector<uint8_t>> command_queue_;
-        int command_send_retries_ = 0;
-        float max_detection_tilt_angle_ = 90;
-        float min_detection_tilt_angle_ = -90;
-        float tilt_angle_margin_ = 5;
-        int16_t max_detection_distance_ = 6000;
-        int16_t max_distance_margin_ = 250;
-        std::vector<Target *> targets_;
-        std::vector<Zone *> zones_;
-        TrackingModeSwitch *tracking_mode_switch_ = nullptr;
-        BluetoothSwitch *bluetooth_switch_ = nullptr;
-        BaudRateSelect *baud_rate_select_ = nullptr;
     };
+
 } // namespace esphome::ld2450
